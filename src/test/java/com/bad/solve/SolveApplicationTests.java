5acbda37aa767c0917456c235a9ab81c2cfc8943
Jaime <jaimegildiaz3838@gmail.com>
2026-06-13 14:43:55 -0600

    Initial Commit

---
+++ src/test/java/com/bad/solve/SolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bad.solve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
